75ecc2fbcf195c84ac44be242284bb3f60bc9454
Riccardo Maria Bianchi <riccardo.maria.bianchi@cern.ch>
2017-11-09 12:14:25 +0100

    moving cards' data to JSON file

---
+++ js/allnotes.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+(function(){
+
+
+//$.getJSON("http://www.riccardomariabianchi.com/atlas-25-memory-game/cards.json", function(json) {
+	$.getJSON("cards.json", function(json) {
+    console.log(json); // this will show the info it in firebug console
+});
+
+
+})();
