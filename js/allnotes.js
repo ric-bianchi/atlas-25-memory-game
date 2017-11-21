63f405d029779cc0fb6dfcc1bc978088ff0f14bc
Riccardo Maria Bianchi <riccardo.maria.bianchi@cern.ch>
2017-11-21 23:54:27 +0100

    styling images

--- js/allnotes.js
+++ js/allnotes.js
@@ -7,6 +7,7 @@
 
 
 	$.getJSON("http://www.riccardomariabianchi.com/atlas-25-memory-game/cards.json", function(json) {
+		
 		console.log(json); // this will show the info it in firebug console
 
 		var cards = json['cards'];
